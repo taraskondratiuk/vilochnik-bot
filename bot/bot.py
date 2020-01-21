7de399393d20ba51b11a8ad7c61bef7528383cc9
Taras Kondratiuk <Taras_Kondratiuk@epam.com>
2020-01-21 17:56:40 +0200

    configured redis in docker-compose.yaml file

--- bot/bot.py
+++ bot/bot.py
@@ -1,12 +1,15 @@
 import telebot
 import os
+import redis
 
 bot = telebot.TeleBot(os.environ['BOT_TOKEN'])
+db = redis.Redis(host='localhost', port=6379)
 
 
 #pytelegrambotapi
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message):
+@bot.message_handler(content_types=["text"], regexp='matches')
+def get_todays_matches_info(message):
+    db.append(message.text, 'cumshot')
     bot.send_message(message.chat.id, message.text)
 
 
