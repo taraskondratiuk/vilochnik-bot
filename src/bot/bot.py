0e258b2dda309662c199683da09290b9988fa2fd
Taras Kondratiuk <Taras_Kondratiuk@epam.com>
2020-01-22 12:51:39 +0200

    pipfile

---
+++ src/bot/bot.py
@@ -0,0 +1,16 @@
+import telebot
+import os
+import redis
+
+bot = telebot.TeleBot(os.environ['BOT_TOKEN'])
+db = redis.Redis(host='localhost', port=6379)
+
+
+@bot.message_handler(content_types=["text"], regexp='matches')
+def get_todays_matches_info(message):
+    db.append(message.text, 'cumshot')
+    bot.send_message(message.chat.id, message.text)
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
