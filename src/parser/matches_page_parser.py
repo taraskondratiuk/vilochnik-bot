ab9c68c31bfc278f25bb8060a62cea703065342e
Taras <kondratiuk.taras123@gmail.com>
2020-01-28 23:52:19 +0200

    started to implement timezones support

--- src/parser/matches_page_parser.py
+++ src/parser/matches_page_parser.py
@@ -1,9 +1,12 @@
 import datetime
+import os
 
 import requests
 from bs4 import BeautifulSoup as Bs
 
 from .single_match_parser import parse as parse_single_match
+
+hours_offset = float(os.environ['UTC_OFFSET'])
 
 
 def get_matches_info():
@@ -12,13 +15,15 @@ def get_matches_info():
     html = Bs(page.content, 'html.parser')
 
     nearest_match_day = html.find('div', class_='match-day')
+    second_nearest_match_day = nearest_match_day.find_next('div', class_='match-day')
 
-    current_date = datetime.datetime.today().strftime('%Y-%m-%d - %A')
+    # current_date = datetime.datetime.today().strftime('%Y-%m-%d - %A')
     nearest_matches_date = nearest_match_day.find('span', class_='standard-headline', recursive=False).text
+    second_nearest_matches_date = second_nearest_match_day.find('span', class_='standard-headline', recursive=False).text
 
-    if current_date == nearest_matches_date:
-        for match in nearest_match_day.find_all('a', class_='a-reset'):
-            if match.find('div', class_='team'):
-                parse_single_match(match['href'], offers_container)
+    # if current_date == nearest_matches_date:
+    for match in nearest_match_day.find_all('a', class_='a-reset'):
+        if match.find('div', class_='team'):
+            parse_single_match(match['href'], offers_container, hours_offset)
 
     return offers_container
--- src/parser/single_match_parser.py
+++ src/parser/single_match_parser.py
@@ -6,12 +6,17 @@ from bs4 import BeautifulSoup as Bs
 from .offer import Offer, add_offer_info_to_container
 
 
-def parse(url, offers_container):
+def parse(url, offers_container, hours_offset):
     url = 'https://www.hltv.org' + url
     page = requests.get(url)
     html = Bs(page.content, 'html.parser')
 
+    unix_time = html.find('div', class_='time')['data-unix']
     match_time = datetime.datetime.strptime(html.find('div', class_='time').text, '%H:%M')
+    print(type(hours_offset))
+    match_date_time = datetime.datetime.fromtimestamp(int(unix_time) / 1000) \
+                      + datetime.timedelta(hours=hours_offset)
+    timezone = datetime.datetime.now().astimezone().tzinfo
     tournament = html.find('div', class_='event text-ellipsis').find('a').text
 
     coef_container = html.find('table', class_='table')
